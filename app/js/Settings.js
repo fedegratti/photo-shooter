707e009b448c4a825bfd65444c0148f72374ca15
Federico Gratti <federicogratti@gmail.com>
2025-02-15 13:06:19 -0300

    Show previous pictures

--- app/js/Settings.js
+++ app/js/Settings.js
@@ -5,6 +5,7 @@ class Settings
     this.debug_mode = false;
     this.dpr = 1;
 
+    this.server_url = 'http://10.0.0.10:8080';
     this.digicam_url = 'http://10.0.0.18:5513';
 
     this.camera = {
--- app/js/views/home/HomeView.js
+++ app/js/views/home/HomeView.js
@@ -8,8 +8,9 @@ import { ViewManager } from 'ohzi-core';
 import home_data from '../../../data/transitions/home.json';
 import { FaceLandmarkerController } from '../../components/FaceLandmarkerController';
 import { SmileDetector } from '../../components/SmileDetector';
+import { Settings } from '../../Settings';
 
-class HomeView extends CommonView
+export class HomeView extends CommonView
 {
   constructor()
   {
@@ -41,6 +42,8 @@ class HomeView extends CommonView
     this.webcam_overlay = document.querySelector('.home__output-canvas');
 
     this.modal = document.querySelector('.home__modal');
+
+    this.previous_pictures = document.querySelector('.home__previous-pictures-list');
   }
 
   before_enter()
@@ -49,6 +52,46 @@ class HomeView extends CommonView
 
     this.scene_controller.before_enter();
     this.transition_controller.before_enter();
+
+    // TODO: Create a node app to work as a proxy to the digicam server
+    fetch(`${Settings.server_url}/session`, {
+      method: 'GET', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      redirect: 'follow' // manual, *follow, error
+    })
+      .then(this.on_session.bind(this))
+      .catch(this.on_error.bind(this));
+  }
+
+  on_session(response)
+  {
+    response.json().then((body) =>
+    {
+      console.log(body);
+      this.previous_pictures.innerHTML = '';
+
+      for (let i = body.Files.length - 1; i >= 0; i--)
+      {
+        const file = body.Files[i];
+
+        const picture_container = document.createElement('div');
+        picture_container.classList.add('home__previous-pictures-item');
+
+        const picture = document.createElement('img');
+        picture.src = `${Settings.digicam_url}/thumb/large/${file.Id}.jpg`;
+        picture.style.transform = `rotate(${file.RotationAngle}deg)`;
+        picture_container.appendChild(picture);
+
+        this.previous_pictures.appendChild(picture_container);
+      }
+    });
+  }
+
+  on_error(error)
+  {
+    console.error(error);
   }
 
   on_enter()
@@ -113,5 +156,3 @@ class HomeView extends CommonView
     // More stuff is happening on FaceLandmarkerController
   }
 }
-
-export { HomeView };
